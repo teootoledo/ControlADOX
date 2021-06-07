a50a422513c99c6bb2dd38e3d2ee8cb9808fe5e1
Teo Martin Toledo <teootoledo@gmail.com>
2021-06-07 13:03:44 -0300

    Initial Commit

---
+++ ControlADOX/ControlADOXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlADOXApp.swift
+//  ControlADOX
+//
+//  Created by Teo Toledo on 07/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct ControlADOXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
